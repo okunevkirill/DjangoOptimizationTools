0ce3cb93df8894fc3a9e689ed9f28c2abade8ea5
ok_kir <clicktmin@gmail.com>
2022-02-01 19:47:55 +0400

    added Backend mail

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -162,8 +162,9 @@ EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Для отладки с python3 -m smtpd -n -c DebuggingServer localhost:25
 # EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
